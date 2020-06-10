9d55007d768b79d769c0f9cb60db0d41adf5bf9f
Jagdeep Manik <jagdeep.manik@berkeley.edu>
2020-06-09 20:28:59 -0700

    fix: Prevent memory leak in NativeEngine

--- Sources/Engine/NativeEngine.swift
+++ Sources/Engine/NativeEngine.swift
@@ -11,15 +11,21 @@ import Foundation
 @available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
 public class NativeEngine: NSObject, Engine, URLSessionDataDelegate, URLSessionWebSocketDelegate {
     private var task: URLSessionWebSocketTask?
+    private var session: URLSession?
     weak var delegate: EngineDelegate?
+    
+    deinit {
+        stop(closeCode: CloseCode.normal.rawValue)
+    }
 
     public func register(delegate: EngineDelegate) {
         self.delegate = delegate
     }
 
     public func start(request: URLRequest) {
-        let session = URLSession(configuration: URLSessionConfiguration.default, delegate: self, delegateQueue: nil)
-        task = session.webSocketTask(with: request)
+        let sessionDelegate = WebSocketDelegateProxy(handler: self)
+        session = URLSession(configuration: .default, delegate: sessionDelegate, delegateQueue: nil)
+        task = session?.webSocketTask(with: request)
         doRead()
         task?.resume()
     }
@@ -27,6 +33,7 @@ public class NativeEngine: NSObject, Engine, URLSessionDataDelegate, URLSessionW
     public func stop(closeCode: UInt16) {
         let closeCode = URLSessionWebSocketTask.CloseCode(rawValue: Int(closeCode)) ?? .normalClosure
         task?.cancel(with: closeCode, reason: nil)
+        session?.invalidateAndCancel()
     }
 
     public func forceStop() {
@@ -75,7 +82,6 @@ public class NativeEngine: NSObject, Engine, URLSessionDataDelegate, URLSessionW
             case .failure(let error):
                 self?.broadcast(event: .error(error))
             }
-            
         }
     }
 
@@ -95,4 +101,33 @@ public class NativeEngine: NSObject, Engine, URLSessionDataDelegate, URLSessionW
         }
         broadcast(event: .disconnected(r, UInt16(closeCode.rawValue)))
     }
+}
+
+/// URLSession holds onto its delegate with a strong reference, so this class
+/// acts as an intermediate handler. The URLSession will hold a strong reference to it,
+/// and this will hold a weak reference to the true delegate, allowing us to gracefully
+/// deinitialize within NativeEngine.
+///
+///     NativeEngine ------> URLSession ------> DelegateProxy
+///         ^        strong             strong        |
+///         |                                         |
+///          ----------------- weak ------------------
+///
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+class WebSocketDelegateProxy: NSObject, URLSessionWebSocketDelegate {
+    
+    weak var delegate: URLSessionWebSocketDelegate?
+    
+    init(handler: URLSessionWebSocketDelegate?) {
+        self.delegate = handler
+    }
+    
+    public func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
+        delegate?.urlSession?(session, webSocketTask: webSocketTask, didOpenWithProtocol: `protocol`)
+    }
+    
+    public func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+        delegate?.urlSession?(session, webSocketTask: webSocketTask, didCloseWith: closeCode, reason: reason)
+    }
+    
 }
